b4835db4975f347c3f80508e8476e0b74a1c95c7
Lizet Lopez <lizet.chlopez@gmail.com>
2025-12-05 23:41:21 -0600

    changed categories

--- routes/categories.py
+++ routes/categories.py
@@ -75,7 +75,7 @@ def get_categories():
     for category in categories:
         category['_id'] = str(category['_id'])
 
-    return jsonify(categories), 200
+    return jsonify({"categories": categories}), 200
 
 # actualizar categoría
 
