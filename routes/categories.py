ed31e92a7ac83f7ac9d623a2e0be81b83a3a8202
Lizet Lopez <lizet.chlopez@gmail.com>
2025-12-06 14:43:23 -0600

    reset categories

--- routes/categories.py
+++ routes/categories.py
@@ -75,7 +75,7 @@ def get_categories():
     for category in categories:
         category['_id'] = str(category['_id'])
 
-    return jsonify({"version": "v2", "categories": categories}), 200
+    return jsonify(categories), 200
 
 
 # actualizar categoría
